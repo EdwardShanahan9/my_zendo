646eeff0494a05af039ee51bdd2ee3c480a27e82
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-04-07 19:22:10 +0100

    Install and configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3a6f78",
+        secondary: "#d85c5a",
+        accent: "#c6b6d6",
+        dark: "#1e1e1e",
+        light: "#eae6e1",
+      },
+      fontFamily: {
+        heading: ['"Playfair Display"', "serif"],
+        body: ['"Poppins"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
