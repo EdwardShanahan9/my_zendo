f30fca5ce97cc9af445885641fa6c7c8e0f57250
Edward Shanahan <193254185+EdwardShanahan9@users.noreply.github.com>
2025-04-16 19:09:00 +0100

    Update Features.jsx

--- src/layouts/Home/Features/Features.jsx
+++ src/layouts/Home/Features/Features.jsx
@@ -31,7 +31,7 @@ const Features = () => {
   return (
     <section>
       <div className="mb-6">
-        <h2 className="text-xl mb-4">Find Clarity in Every Task</h2>
+        <h2 className="">Find Clarity in Every Task</h2>
 
         <p className="text-sm">
           Zendo brings focus and balance to your work, helping you stay
